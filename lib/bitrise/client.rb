d2180016fa9e9a837250363209d2213091e5fd89
Takuma Homma <nagomimatcha@gmail.com>
2019-01-24 00:41:22 +0900

    Initialized

---
+++ lib/bitrise/client.rb
@@ -0,0 +1,8 @@
+require "bitrise/client/version"
+
+module Bitrise
+  module Client
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/bitrise/client/version.rb
@@ -0,0 +1,5 @@
+module Bitrise
+  module Client
+    VERSION = "0.1.0"
+  end
+end
