673e85c45aa0d28d6b713cf2bc368842868cb524
Takuma Homma <nagomimatcha@gmail.com>
2019-01-27 22:55:16 +0900

    Load by `require 'bitrise'`

---
+++ lib/bitrise.rb
@@ -0,0 +1,1 @@
+require 'bitrise/client'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
 require "bundler/setup"
-require "bitrise/client"
+require 'bitrise'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
