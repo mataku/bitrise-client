4d9bf080b9150ee13fd82532de7a3dad45c058c5
Takuma Homma <nagomimatcha@gmail.com>
2019-01-27 23:04:42 +0900

    :wave: Sample test

--- spec/bitrise/client_spec.rb
+++ spec/bitrise/client_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Bitrise::Client do
   it "has a version number" do
     expect(Bitrise::Client::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
