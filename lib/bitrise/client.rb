4f95082e154298ef7c1a80d2c3306fc4b79a520b
Takuma Homma <nagomimatcha@gmail.com>
2019-01-27 20:51:47 +0900

    Use faraday as client

--- lib/bitrise/client.rb
+++ lib/bitrise/client.rb
@@ -1,9 +1,10 @@
+require 'faraday'
+
 module Bitrise
   class Client
     def initialize(options = {})
-      @api_host = options[:host] || 'https://api.bitrise.io/v0.1'
+      @api_host = options[:host] || 'https://app.bitrise.io'
       @timeout = options[:timeout] || 30
-      @api_token = options[:api_token] || raise('API token required')
       @open_timeout = options[:open_timeout] || 30
     end
 
@@ -11,10 +12,12 @@ module Bitrise
 
     def client
       @client ||= Faraday.new(url: @api_host) do |faraday|
-        faraday.adapter Faraday.default_adapter
         faraday.options.timeout        = @timeout
         faraday.options.open_timeout   = @open_timeout
+        faraday.options.params_encoder = Faraday::FlatParamsEncoder
         faraday.response :logger
+
+        faraday.adapter Faraday.default_adapter
       end
     end
   end
