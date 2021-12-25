0a2f437318198c2ddef4ec1309acdd2480b38a53
Takuma Homma <nagomimatcha@gmail.com>
2021-12-25 22:42:43 +0900

    Enable to abort a build

--- lib/bitrise/client/build.rb
+++ lib/bitrise/client/build.rb
@@ -31,5 +31,29 @@ module Bitrise
         BuildTriggerResult.new(result)
       end
     end
+
+    # Abort a build of your bitrise app
+    #
+    # @param app_slug [String] Your bitrise app slug
+    # @param build_slug [String] Bitrise build slug which you want to abort
+    #
+    # @param options [Hash]
+    # @option opts [String]  :abort_reason        A reason for aborting the build
+    # @option opts [Boolean] :abort_with_success  Treat as a successful or not
+    # @option opts [Boolean] :skip_notifications  Set true if you want to send email notifications about aborting by Bitrise
+    #
+    # @return [Bitrise::BuildTriggerResult]
+    #
+    # See: https://devcenter.bitrise.io/en/api/triggering-and-aborting-builds.html#aborting-a-build
+    def abort_build(app_slug: nil, build_slug: nil, options: {})
+      raise ArgumentError, 'App slug required. You must specify by \'app_slug:\'' unless app_slug
+      raise ArgumentError, 'Build slug required. You must specify by \'build_slug:\'' unless build_slug
+
+      client.post do |request|
+        request.url "/v0.1/apps/#{app_slug}/builds/#{build_slug}/abort"
+        request.headers['Content-Type'] = 'application/json'
+        request.body = options.to_json
+      end
+    end
   end
 end
