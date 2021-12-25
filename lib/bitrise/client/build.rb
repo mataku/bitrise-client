57fe058321cf6392ba086b5bddd78f3e336d5578
Takuma Homma <nagomimatcha@gmail.com>
2021-12-25 22:41:59 +0900

    Correct description

--- lib/bitrise/client/build.rb
+++ lib/bitrise/client/build.rb
@@ -7,11 +7,11 @@ module Bitrise
       # Trigger a build of your bitrise app
       #
       # @param app_slug [String] Your bitrise app slug
-      # @param access_token [String] Access token
+      # @param build_params [Hash] Bulld params so that Bitrise can identify which workflow to run. Specify a branch or tag or workflow_id.
       #
       # @return [Bitrise::BuildTriggerResult]
       #
-      # See: https://devcenter.bitrise.io/api/build-trigger/
+      # See: https://devcenter.bitrise.io/en/api/triggering-and-aborting-builds.html#triggering-a-new-build-with-the-api
       def trigger_build(app_slug: nil, build_params: {})
         raise ArgumentError, 'App slug required. You must specify by \'app_slug:\'' unless app_slug
         raise ArgumentError, 'No value found for \'branch\' or \'tag\' or \'workflow_id\'' if build_params.empty?
